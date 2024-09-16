836d32f79aaa162efca276b61e3e9dd66821e905
Huynh Phan <phanhuynh681@gmail.com>
2024-09-17 00:36:37 +0700

    React Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+// class component
+//functional component
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My first Component</h1>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
