88498cffc8861067fb72b8e97f77809e4e2a8a47
Huynh Phan <phanhuynh681@gmail.com>
2024-11-07 16:27:08 +0700

    start code react hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import Test from "./components/Test";
-import React from "react";
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <div>
-          <MyComponent />
-        </div>
-      </>
-    );
-  }
-}
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
+import "./App.scss";
 
-//   return (
-//     <>
-//       <div>
-//         <MyComponent/>
-//       </div>
-//     </>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <div>
+//           <MyComponent />
+//         </div>
+//       </>
+//     );
+//   }
 // }
+
+const App = () => {
+  return (
+    <>
+      <div className="app-container">
+        
+      </div>
+    </>
+  );
+};
 export default App;
