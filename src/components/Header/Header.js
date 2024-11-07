e890a47d88d6ee81dc921b1a7735c8cb2d743c48
Huynh Phan <phanhuynh681@gmail.com>
2024-11-07 18:41:40 +0700

    video50 Active NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../../../src/logo.svg";
 import { CiSettings } from "react-icons/ci";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -23,9 +23,9 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to='/' className="nav-link">Home</Link>
-            <Link to='/users' className="nav-link">Users</Link>
-            <Link to='/admin' className="nav-link">Admin</Link>
+            <NavLink to='/' className="nav-link">Home</NavLink>
+            <NavLink to='/users' className="nav-link">Users</NavLink>
+            <NavLink to='/admin' className="nav-link">Admin</NavLink>
           </Nav>
           <Nav>
           <NavDropdown title={
