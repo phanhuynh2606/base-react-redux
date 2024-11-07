1f9ed0a79f25ff154b707cdb93ea84719a61415e
Huynh Phan <phanhuynh681@gmail.com>
2024-11-07 18:18:01 +0700

    Video 48 Links

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,10 @@
+
+
+const Admin = (props) =>{
+  return <>
+    <div>
+    Admin components
+    </div>
+  </>
+}
+export default Admin;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../../../src/logo.svg";
 import { CiSettings } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -22,9 +23,9 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Users</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
+            <Link to='/' className="nav-link">Home</Link>
+            <Link to='/users' className="nav-link">Users</Link>
+            <Link to='/admin' className="nav-link">Admin</Link>
           </Nav>
           <Nav>
           <NavDropdown title={
