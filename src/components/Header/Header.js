eb8e6cd22e466311a92edf64e35aaa22a894bfd3
Huynh Phan <phanhuynh681@gmail.com>
2024-11-08 16:30:43 +0700

    Video 52 New header and homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,16 +22,8 @@ const Header = () => {
             <NavLink to='/admin' className="nav-link">Admin</NavLink>
           </Nav>
           <Nav>
-          <NavDropdown title={
-            <span>
-            <CiSettings className="me-2" />
-            Setting
-          </span>
-          } id="basic-nav-dropdown">
-              <NavDropdown.Item >Log in</NavDropdown.Item>
-              <NavDropdown.Item >Log out</NavDropdown.Item>
-              <NavDropdown.Item >Profile</NavDropdown.Item>
-            </NavDropdown>
+              <button className="btn-login"> Log in</button>
+              <button className="btn-signup"> Sign up</button>
           </Nav>
         </Navbar.Collapse>
       </Container>
