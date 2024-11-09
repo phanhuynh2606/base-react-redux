9e51b3ba35a89ead7044c131dc32b46eb93842b1
Huynh Phan <phanhuynh681@gmail.com>
2024-11-09 15:23:18 +0700

    Video 59 Validate data & React Toastify

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FaPlusCircle } from "react-icons/fa";
 import axios from "axios";
+import { toast } from "react-toastify";
 const ModalCreateUser = (props) => {
   const {show,setShow} = props;
 
@@ -31,16 +32,25 @@ const ModalCreateUser = (props) => {
       // setPreviewImage("");
     }
   }
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
+  //submit dataform
   const handleSubmitCreateUser = async () => {
-    // let data = {
-    //   email : email,
-    //   password: password,
-    //   username : username,
-    //   role : role,
-    //   userImage: image
-    // }
-    // console.log(data);
+    const checkEmail = validateEmail(email);
+    if(!checkEmail){
+      toast.error('Invalid Email!');
+      return;
+    }
+    if(!password){
+      toast.error('Password is required!');
+      return;
+    }
     const data = new FormData();
     data.append("email", email);
     data.append("password", password);
@@ -49,16 +59,17 @@ const ModalCreateUser = (props) => {
     data.append("userImage", image);
     // call API create user
     let res = await axios.post("http://localhost:8081/api/v1/participant", data);
-    console.log(res);
-    
+    if(res.data && res.data.EC === 0){
+      toast.success(res.data.EM);
+      handleClose();
+    }
+    if(res.data && res.data.EC !== 0){
+      toast.error(res.data.EM);
+    }
   }
 
   return (
     <>
-      {/* <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
-
       <Modal
         show={show}
         onHide={handleClose}
