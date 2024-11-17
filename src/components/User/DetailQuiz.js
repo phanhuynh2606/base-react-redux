4e39fd1a64cc36b602129eef66de2646e537f566
Huynh Phan <phanhuynh681@gmail.com>
2024-11-18 02:46:28 +0700

    Video 82 - Xu li du lieu phia frontend nho lodash

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -15,6 +15,24 @@ const DetailQuiz = () => {
     const res = await getDataQuiz(quizId);
     if(res && res.EC === 0){
       let raw = res.DT;
+      let data = _.chain(raw)
+    // Group the elements of Array based on `color` property
+    .groupBy("id")
+    // `key` is group's name (color), `value` is the array of objects
+    .map((value, key) => {
+      let answers = [];
+      let questionDescription, image = null;
+      value.forEach((item,index) => {
+        if(index === 0){
+          questionDescription = item.description;
+          image = item.image;
+        }
+        answers.push(item.answers);
+      })
+      return { questionId: key, answers,questionDescription, image }
+    })
+    .value();
+    console.log(data);
     }
   }
 
