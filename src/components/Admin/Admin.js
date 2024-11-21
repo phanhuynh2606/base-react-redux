834ffc5229ab863fa9fccac008b1e20daf057803
Huynh Phan <phanhuynh681@gmail.com>
2024-11-22 05:28:19 +0700

    Fix loi ScrollBar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,26 +1,28 @@
 import SideBar from "./SideBar";
 import "./Admin.scss";
-import {FaBars} from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-
-const Admin = (props) =>{
+import PerfectScrollbar from "react-perfect-scrollbar";
+const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
     <div className="admin-container">
       <div className="admin-sidebar">
-        <SideBar collapsed={collapsed}/>
+        <SideBar collapsed={collapsed} />
       </div>
       <div className="admin-content">
         <div className="admin-header">
-          <FaBars onClick={() =>setCollapsed(!collapsed)}/>
+          <FaBars onClick={() => setCollapsed(!collapsed)} />
         </div>
         <div className="admin-main">
-            <Outlet/>
+          <PerfectScrollbar>
+            <Outlet />
+          </PerfectScrollbar>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Admin;
